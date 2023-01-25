ed44a581888a27049fc9d9b35ebfe113229d4504
marynazakvatska <maryna.zakvatska@gmail.com>
2023-01-25 21:47:24 +0100

    modal

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
 
 
 
-  const refs = {
+ /*  const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
-   /*  background: document.querySelector('.overlay_modal_card') */
+ 
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
- /*  refs.background.addEventListener('click', toggleModal); */
+
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
+ */
+
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    backdrop: document.querySelector('.js-backdrop'),
+}
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal() {
+    document.addEventListener('keydown', onEscapeClick)
+    document.body.classList.add('show-modal');
+}
+
+function onCloseModal() {
+    document.removeEventListener('keydown', onEscapeClick)
+    document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(evt) {
+    if (evt.currentTarget === evt.target) {
+        onCloseModal();
+    }
+}
+
+function onEscapeClick(evt) {
+    if (evt.code === 'Escape') {
+        onCloseModal();
+    }
+}
