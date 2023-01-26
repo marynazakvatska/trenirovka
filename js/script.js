caee66066094c76f9550b3835c12f2757b818bec
marynazakvatska <maryna.zakvatska@gmail.com>
2023-01-26 10:02:10 +0100

    knopki

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
 
 
-
- /*  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
- 
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
-
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
- */
-
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    backdrop: document.querySelector('.js-backdrop'),
+      backdrop: document.querySelector('.js-backdrop'),
+      addBtn: document.querySelector(".button_card_add")
+    
 }
 
 refs.openModalBtn.addEventListener('click', onOpenModal);
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onBackdropClick);
+ refs.addBtn.addEventListener("click", addToLocalStorage) 
 
 function onOpenModal() {
     document.addEventListener('keydown', onEscapeClick)
@@ -48,3 +34,22 @@ function onEscapeClick(evt) {
         onCloseModal();
     }
 }
+
+/* https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>> */
+/* /movie/{movie_id} */
+/* API Key (v3 auth): 671c14eb1babf71c7ecd9b35ab5716a8 */
+
+function fetchOneMovie() {
+  return  fetch("https://api.themoviedb.org/3/movie/76341?api_key=671c14eb1babf71c7ecd9b35ab5716a8")
+        .then(resp => resp.json())
+    .then(data=>console.log(data.id))
+  
+}
+
+fetchOneMovie()
+
+function addToLocalStorage() {
+localStorage.setItem("key", JSON.stringify(fetchOneMovie) )
+}
+
+/* addToLocalStorage() */
